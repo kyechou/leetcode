c56f1aeae35f09c2c5b64e8111c5d1a04321866b
kyechou <kuanyenchou@gmail.com>
2022-11-30 12:49:57 -0600

    Add template code

---
+++ src/template.cpp
@@ -0,0 +1,44 @@
+/**
+ * 1. Title
+ *
+ * Description
+ *
+ * Notes
+ */
+
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace std;
+
+/**
+ * n: length of the array
+ * Time: O(n^2)
+ * Space: O(n)
+ */
+class Solution1 {
+public:
+    ;
+};
+
+/**
+ * n: length of the array
+ * Time: O(n)
+ * Space: O(n)
+ */
+class Solution2 {
+public:
+    ;
+};
+
+TEST_CASE("Title") {
+    // Solution1 s1;
+    // Solution2 s2;
+
+    SECTION("") {
+        // vector<int> nums = {2, 7, 11, 15};
+        // REQUIRE(s1.twoSum(nums, 9) == vector<int>({0, 1}));
+        // REQUIRE(s2.twoSum(nums, 9) == vector<int>({0, 1}));
+    }
+}
